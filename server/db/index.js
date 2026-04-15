10fe39e93063534a13cca4ef3ee04ef911eb9dce
rahulcunni <rahulanoopcl@example.com>
2026-04-15 14:23:11 +0530

    Fix Supabase pooler connection

--- server/db/index.js
+++ server/db/index.js
@@ -3,6 +3,7 @@ const { Pool } = require('pg');
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false },
+  max: 1,
 });
 
 const initDB = async () => {
